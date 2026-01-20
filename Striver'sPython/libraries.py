2430ecd1cf4ee32cba225bf7c5e399954e93febb
mishrp13 <prabalsonu1997@gmail.com>
2026-01-20 18:29:24 +0530

    new changes

---
+++ Striver'sPython/libraries.py
@@ -0,0 +1,63 @@
+
+
+# def length_of_string(raj):
+#     cnt=0
+#     for ele in raj:
+#         cnt=cnt+1
+#     return cnt
+
+# name="raj"
+
+# print(length_of_string(name))
+#print(len(name))
+
+
+# Built in function
+
+# arr = [1,5,6,7,4]
+# print(sorted(arr, reverse=True))
+# print(arr)
+
+# arr=[-1,2,5,-6,7,4]
+# print(sorted(arr, key=lambda x: abs(x)))
+# print(arr)
+
+
+# fruit_list=["apple","banana","kiwi"]
+# print(sorted(fruit_list,reverse=True, key=len))
+
+#arr=[5,6,7,8,-15]
+# print(max(arr, key=lambda x : abs(x)))
+
+# fruit_list= ["orange", "kiwi", "pineapple"]
+# print(max(fruit_list, key=len))
+
+#print(sum(arr))
+
+# arr = [10,20,30]
+# print(sum(arr,start=10))
+
+# import math
+# arr= [10,20,30]
+# print(math.prod(arr))
+
+# arr = [True, False, True]
+# print(any(arr))
+# print(all(arr))
+
+arr = [1,2,3,4,4,5]
+print(arr.count(4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
