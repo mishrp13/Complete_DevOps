f1433b230829e50dfaf871ae44c39a6a089209e2
mishrp13 <prabalsonu1997@gmail.com>
2026-01-22 18:37:04 +0530

    new changes

---
+++ Python/practise.py
@@ -0,0 +1,10 @@
+
+print(f"20 days are {20*24*60} minutes")
+print(f"34 days are {34*24*60} minutes")
+
+
+calculation_to_seconds=24*60*60
+name_of_unit="seconds"
+
+
+print(f"50 days are {50*calculation_to_seconds} {name_of_unit}")
