a5c79348d34a0e60735ed93210aec3d58ff7f535
mishrp13 <prabalsonu1997@gmail.com>
2026-03-13 09:13:30 +0530

    new changes

---
+++ DevOps_Python/Shradha_Python/Lecture1.py
@@ -0,0 +1,29 @@
+# first= int(input("enter any number: "))
+# second= int(input("enter any number: "))
+
+# sum= first + second
+# print(f"sum of number is {sum}")
+
+
+# second Question
+
+# side= float(input("enter the side of square: "))
+# print(f" side of sqare is : {side*side}")
+
+
+# Third question
+
+# a= float(input("enter any number: "))
+# b= float(input("enter any number: "))
+
+# print(f"Average of number is : {(a+b)/2}")
+
+# Fourth question
+
+a= int(input("enter any number: "))
+b= int(input("enter any number: "))
+
+print(f"{a>b}")
+
+
+
---
+++ DevOps_Python/Shradha_Python/Lecture2.py
@@ -0,0 +1,15 @@
+# string concat
+
+# string1= "Prabal"
+# len1= len(string1)
+# print(len1)
+# string2= "Mishra"
+# len2=len(string2)
+# print(len2)
+# print(f"{string1+" "+string2}")
+
+
+
+str = "Prabal Mishra"
+ch =str[0]
+print(ch)
