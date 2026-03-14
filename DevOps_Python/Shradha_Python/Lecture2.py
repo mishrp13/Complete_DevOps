bd3b2a34c1ba217e4f6f344646e0d366d7bef86c
mishrp13 <prabalsonu1997@gmail.com>
2026-03-14 09:20:00 +0530

    new changes

--- DevOps_Python/Shradha_Python/Lecture2.py
+++ DevOps_Python/Shradha_Python/Lecture2.py
@@ -10,6 +10,67 @@
 
 
 
-str = "Prabal Mishra"
-ch =str[0]
-print(ch)
+# str = "Prabal Mishra"
+# ch =str[0]
+# print(ch)
+
+# str= "Prabal Mishra"
+# print(str[3:4])
+# print(str[0:len(str)])
+# print(str[0:])
+# print(str[:4])
+# print(str[-3:-1])
+
+#
+# name= input("enter your name ")
+# print(f"Length of your string is {len(name)}")
+
+#Occcurence in string
+
+# str= "Hi , 1 am $ and $ is $"
+# print(f"count of $ is {str.count("$")}")
+
+# age= int(input("enter any number "))
+
+# if (age>18):
+#     if (age>80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+
+
+# num = int(input("enter any number: "))
+
+# rem = num%2
+
+# if (rem==0):
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+
+
+
+# a =int(input("enter the number: "))
+# b =int(input("enter the number: "))
+# c =int(input("enter the number: "))
+
+# if (a>=b and a>=c):
+#     print(f"a is greatest, {a}")
+# elif (b>=c):
+#     print(f"b is greatest, {b}")
+# else:
+#     print(f"c is greatest, {c}")
+
+
+x = int(input("enter any number: "))
+
+if(x % 7  == 0):
+    print(" x is multiple of 7")
+else:
+    print(" x is not a multiple of 7")
+
+
+
---
+++ DevOps_Python/Shradha_Python/Lecture3.py
@@ -0,0 +1,45 @@
+# marks= [94.4, 87.5, 95.2, 66.4, 45.1]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+
+
+# student = ["Karan", 95.4, 17, "Delhi"]
+# print(student)
+
+#***Strings are immutable in python ******
+#****Lists are mutable in Python ******
+#**Tuples are also immutable ***
+
+# student[0]= "Arjun"
+# print(student)
+# print(student[1:3])
+# print(student[-3:-1])
+
+# tup =(2,1,3,4)
+# print(type(tup))
+# print(tup[0])
+# print(tup[1])
+
+# movies = []
+
+# mov1= input("enter the first movie:")
+# movies.append(mov1)
+# mov2= input("enter the second movie:")
+# movies.append(mov2)
+# mov3= input("enter the third movie:")
+# movies.append(mov3)
+
+# print(movies)
+
+
+
+
+
+
+
+
+
+
+
