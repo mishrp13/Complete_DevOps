119b97311b783f7e75756c0a0bd7e2c286c28b8b
mishrp13 <prabalsonu1997@gmail.com>
2026-03-17 12:50:27 +0530

    new changes

---
+++ DevOps_Python/Shradha_Python/Lecture5.py
@@ -0,0 +1,229 @@
+# count =1
+
+# while count <=5:
+#     print("hello world",count)
+#     count +=1
+
+# print(count)
+
+#-----------------------------------
+
+# Print from 1 to 5
+
+
+
+# i=1
+
+# while i<=5:
+#     print(i)
+#     i+=1
+
+# print("Loop ended")
+
+
+#---------------------------------------
+
+# print from 5 to 1
+
+# i =5
+
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# print("Loop ended")
+
+
+#--------------------------------
+# print number from 1 to 100
+
+# i=1
+
+# while i <=100:
+#     print(i)
+#     i+=1
+# print("Loop ended")
+
+# #-----------------------------------
+# #Print a number from 100 to 1
+
+# i=100
+
+# while i>=1:
+#     print(i)
+#     i -=1
+
+# print("Loop ended")
+
+#------------------------------
+# Print a multiplication table of a number n
+
+# n = int(input("enter the number: "))
+# i=1
+
+# while i <= 10:
+#     print(n*i)
+#     i +=1
+
+
+#-------------------------------------------
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# idx=0
+
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1
+
+# -------------------------------------------
+
+# heroes= ["superman","shaktiman","JuniorG","Karma"]
+
+# idx=0
+
+# while idx < len(heroes):
+#     print(heroes[idx])
+#     idx +=1
+
+#----------------------------------------------------
+# nums = (1,4,6,9,12,34,56,76,34)
+
+# x=34
+
+# i=0
+
+# while i < len(nums):
+#     if (nums[i]==x):
+#         print("num found at idx",i)
+#         break
+#     else:
+#         print("Finding.........")
+#     i +=1
+
+# print("end of Loop")
+
+
+#---------------------------------------------
+
+# i=1
+
+# while i <= 5:
+#     print(i)
+#     if (i==3):
+#         break
+#     i += 1
+
+# print("End of loop")
+
+
+#----------------------------------------
+
+# i = 0
+
+# while i <= 10:
+#     if (i%2==0):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+#--------------------------------
+
+# veggies = ["Potato","tomato","LadyFinger"]
+
+# for val in veggies:
+#     print(val)
+
+#----------------------------------------------
+
+# str = "prabalMishra"
+
+# for char in str:
+#     if (char == "M"):
+#         print("O found")
+#         break
+#     print(char)
+# else:
+#     print("end")
+
+#----------------------------------------
+
+# nums = (1,2,5,6,7,8,9,98,45,67,6)
+
+# idx=0
+# x=6
+# for el in nums:
+#     if (el == x):
+#         print("number found at idx", idx)
+#         break
+#     idx += 1
+
+#-------------------------------------------------
+
+# for i in range(2,101,2):
+#     print(i)
+
+#----------------------------
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+#-------------------------------
+
+# n = int(input("enter the number: "))
+
+# for i in range(1,11):
+#     print(n*i)
+
+#---------------------------------------
+
+# n = 7
+
+# sum =0
+
+# for i in range(1, n+1):
+#     sum += i
+
+# print("Toral sum", sum)
+
+
+#-----------------------------------------
+
+# n =4
+
+# factorial =1
+# i=1
+
+# while i<=n:
+#     factorial *=i
+#     i += 1
+# print(factorial)
+
+#-------
+
+n = 5
+factorial =1
+
+for i in range(1, n+1):
+    factorial *= i
+
+print(factorial)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
